5a5512388006cfc545a5b3138c47bc1e8eb8dd5b
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2019-01-23 00:30:13 +0400

    video autplay autopause

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
+let iframe
+let iframePlayer
+
 new fullpage('#fullpage', {
     //anchors:['coversee', 'coverhear', 'coverspeak','coveract'],
     //menu: '#nav'
+    afterLoad: function(origin, destination, direction){
+		//var loadedSlide = this;
+        
+        if(destination.anchor=="speak-video"){
+            iframe = document.querySelector('#video_video_594');
+            iframePlayer = new Vimeo.Player(iframe);
+            iframePlayer.play();
+        }
+
+        if(origin.anchor=="speak-video"){
+            iframePlayer.pause();
+        }
+
+    }
+
+    ,onLeave: function(origin, destination, direction){
+        //console.log(iframePlayer)
+        /*
+        if(typeof(iframePlayer)){
+            iframePlayer.stop()
+        }
+        */
+        
+
+	}
+    
 });
