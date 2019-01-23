61c82b39d1eb8896abf6dc53ab4c0995b9e3be13
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2019-01-23 09:22:58 +0000

    quote fades

--- js/script.js
+++ js/script.js
@@ -5,17 +5,29 @@ new fullpage('#fullpage', {
     //anchors:['coversee', 'coverhear', 'coverspeak','coveract'],
     //menu: '#nav'
     afterLoad: function(origin, destination, direction){
-		//var loadedSlide = this;
+		let loadedSlide = this;
         
+        
+        /* Quotes fadeIn */
+        let quote = $("#"+destination.item.id).find(".quote")
+        let quote_autor = $("#"+destination.item.id).find(".quote-autor")
+        if(quote && quote_autor){
+            $(quote[0]).addClass("fadeIn2")
+            $(quote_autor[0]).addClass("fadeIn8")
+        }
+        /* end of Quotes fadeIn */
+
+        /* Video */
         if(destination.anchor=="speak-video"){
             iframe = document.querySelector('#video_video_594');
             iframePlayer = new Vimeo.Player(iframe);
             iframePlayer.play();
         }
 
-        if(origin.anchor=="speak-video"){
+        if(origin && origin.anchor=="speak-video"){
             iframePlayer.pause();
         }
+        /* end of Video */
 
     }
 
@@ -27,7 +39,6 @@ new fullpage('#fullpage', {
         }
         */
         
-
 	}
     
 });
