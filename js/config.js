6dac362416663d4ee91e5e8451ec729d7d67eba8
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2019-03-12 19:59:56 +0400

    facebook scripts, etc ...

--- js/config.js
+++ js/config.js
@@ -7,6 +7,23 @@ const INDEXPAGE = (LANG=="en")? "/" : "index_hy.html";
 var volume = 0;
 
 
+window.fbAsyncInit = function () {
+  FB.init({
+      appId: '1774450775918270',
+      xfbml: true,
+      version: 'v2.10'
+  });
+  FB.AppEvents.logPageView();
+};
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) { return; }
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
 
 //share buttons 
 document.getElementById('fb-button').onclick = function() {
