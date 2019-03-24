5c9023099d204112389e874c1330e9d58226d7f9
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2019-03-24 23:30:04 +0400

    mobile concept

--- js/functions.js
+++ js/functions.js
@@ -2,17 +2,56 @@ const TOP_OFFSET_SLIDELINE = 50;
 const BOTTOM_OFFSET_SLIDELINE = 55;
 const SLIDELINE_POINT_HEIGHT = 18;
 const POINTHEIGTH = 5
-const TIME_BEFORE_SLIDELINE = 1000; 
+const TIME_BEFORE_SLIDELINE = 1000;
+const MOBILE_MAX_WIDTH = 425;
 
+const IS_MOBILE = checkMobile() //checkMobile() 
 
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function checkMobile() {
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+    /*
+    if (window.innerWidth <= MOBILE_MAX_WIDTH) {
+        return true;
+    } else {
+        return false;
+    }
+    */
 }
 
+function toggleMobileForRelatives() {
+    if (IS_MOBILE) {
+        let zoomerIndex = 0
+        let relativeIndex = 0
 
-function setSound() {
-    for (video of videosList) {
-        video.volume = volume
+        screen.orientation.lock('landscape');
+        $(".zoomer").each(function (i, zoomer) {
+            $(zoomer).hide()
+
+            let relatives = $(zoomer).find(".relatives, .relatives-fade")
+            $(relatives).each(function (z, relative) {
+                let isTTL = ($(relative).data("src") || "").indexOf("ttl") > -1
+                if (isTTL) {
+                    $(relative).addClass("hidden-for-mobile")
+                }
+                else {
+                    let reltvs = $(relative).addClass("relatives-for-mobile")
+                    let slide = $("<div>")
+                                        .addClass("slide")
+                                        .attr("id","slideID"+zoomerIndex+"_"+relativeIndex)
+                                        .append(reltvs)
+                    $(zoomer).after(slide) 
+                }
+
+                relativeIndex++;
+            })
+
+            zoomerIndex++;
+        })
+
+
+        $(".zoomer").remove()
     }
 }
 
@@ -190,9 +229,9 @@ function scrollInfo(delta = 5, lastScrollInfo, callback) {
 
 }
 
-function openSlideline(anchor){
+function openSlideline(anchor) {
     const chapetrEnds = ["s16", "s33", "s50"]
-    menuForCurrentPageForced = (chapetrEnds.indexOf(anchor)>-1)?true:false;
+    menuForCurrentPageForced = (chapetrEnds.indexOf(anchor) > -1) ? true : false;
     return menuForCurrentPageForced
 }
 
@@ -247,7 +286,7 @@ function setStateSlideLinePoints(p) {
     if (menuOpened) {
         p.css({ "display": "block" })
     }
-    else if (menuForCurrentPageForced){
+    else if (menuForCurrentPageForced) {
         p.css({ "display": "block" })
     } else {
         p.css({ "display": "none" })
@@ -278,7 +317,7 @@ function initScale() {
     $(window).on("load", changeRatio)
     $(window).on("resize", slideLine)
     $(window).on("resize", changeRatio)
-   
+
 }
 
 function savePositionsOfRelatives() {
@@ -290,19 +329,21 @@ function savePositionsOfRelatives() {
 
 
 function changeRatio() {
-    browserWidth = $(window).width();
-    browserHeight = $(window).height();
-    let ratio = (browserWidth / INITIAL_SIZE).toFixed(2);
-    let centerINIT = INITIAL_SIZE_HEIGHT / 2
-    let centerNEW = browserHeight / 2
-    let MAGIC_NUMBER = 450
+    if (!IS_MOBILE) {
+        browserWidth = $(window).width();
+        browserHeight = $(window).height();
+        let ratio = (browserWidth / INITIAL_SIZE).toFixed(2);
+        //let centerINIT = INITIAL_SIZE_HEIGHT / 2
+        //let centerNEW = browserHeight / 2
+        let MAGIC_NUMBER = 450
 
-    $(".zoomer").css({
-        "transform": "scale(" + ratio + ") translate(0px, -" + MAGIC_NUMBER + "px)",
-        "transform-origin": "top left",
-    })
+        $(".zoomer").css({
+            "transform": "scale(" + ratio + ") translate(0px, -" + MAGIC_NUMBER + "px)",
+            "transform-origin": "top left",
+        })
 
-    adjustPersonSlides()
+        adjustPersonSlides()
+    }
 }
 
 
@@ -480,11 +521,11 @@ function initOpacityBackgrounds() {
 function initBxGallery(elem) {
     elem.html("")
     let arrayName = elem.data("han-src-array")
-    for(let imgSrc of GALLERYSLIDERIMAGES[arrayName]){
+    for (let imgSrc of GALLERYSLIDERIMAGES[arrayName]) {
         let divWithImage = $("<div>")
-                                .addClass("slider-image-bkgr")
-                                .css("background-image","url('"+imgSrc+"')")
-        
+            .addClass("slider-image-bkgr")
+            .css("background-image", "url('" + imgSrc + "')")
+
         elem.append(divWithImage)
     }
 
@@ -562,9 +603,7 @@ function initCoverPageEffects() {
     const IMAGE_TIMEOUT = 1000;
     const TIME_BEFORE_START = 1500;
 
-    window.onload = function () {
-
-
+    $(document).ready(function () {
         $(".cover-text").fadeOut(TIME_BEFORE_START)
         $("#spinner").fadeOut(TIME_BEFORE_START)
 
@@ -593,9 +632,9 @@ function initCoverPageEffects() {
             }, LINE_TIME_OUT)
 
         }, TIME_BEFORE_START)
-    }
+    })
 
-    function showScrollbarAndNavArrow(){
+    function showScrollbarAndNavArrow() {
         $("#scrolldown").show("slow", function () {
             $("#nav-arrow").show("slow");
         });
@@ -698,4 +737,15 @@ function updateLanguageLink(currentAnchor) {
     let linkStart = href.split("#")[0]
     linkStart += "#" + currentAnchor
     $(link).attr("href", linkStart)
+}
+
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function setSound() {
+    for (video of videosList) {
+        video.volume = volume
+    }
 }
--- js/script.js
+++ js/script.js
@@ -7,277 +7,285 @@ let menuForCurrentPageForced = false;
 let videosList = [];
 let galleryes = [];
 
-setSound()
 initScale()
 initMenuAndTools()
-//savePositionsOfRelatives()
 
 $(function () {
     initIntro()
     initHome()
     initOpacityBackgrounds()
-    initToolTips()
+
+
+    if (IS_MOBILE) {
+        //toggleMobile()
+        toggleMobileForRelatives()
+    }
+    else {
+        initToolTips()
+    }
 })
 
+setTimeout(function () {
 
-//alert("loaded window")
-//console.log("")
-
-
-new fullpage('#fullpage', {
-    fadingEffect: true,
-    fadingEffectKey: "dmVsdmV0c2tldGNoZXMubWVkaWEuYW1fbDRmWm1Ga2FXNW5SV1ptWldOMGxTNQ==",
-    verticalCentered: true,
-    lazyLoading: true,
-    afterLoad: function (origin, destination, direction) {
-        if (destination.item.id === "cover") {
-            initCoverPageEffects();
-        }
-        else{
-            $("#nav-arrow").show("slow");
-        }
-
-        if (destination.item.id == "expertise") {
-            window.location.assign(EXPERTISEPAGE)
-        }
-
-        let slide = $("#" + destination.item.id);
-
-        let customVideo = $(slide).find(".custom-video")
-        if (customVideo.length != 0) {
-            videosList = prepareVideos(customVideo)
-            videosList[0].on("timeupdate", function (evnt) {
-                if (videosList[0].currentTime > 0 && videosList[0].currentTime > videosList[0].duration - 0.3) {
-                    $(slide).find(".plyr__control").hide()
-                    videosList[0].pause()
-                }
-            })
-        }
-
-
-        let fallVideo = $(slide).find(".fall-video")
-        if (fallVideo.length != 0) {
-            videosList = prepareVideos(fallVideo, { 'autopause': false }, false)
-        }
-
-        let personVideo = $(slide).find(".personVideo")
-        if (personVideo.length != 0) {
-            videosList = prepareVideos(personVideo, { 'autopause': false })
-        }
-
-        if (LANG === "en") {
-            let tb = $(slide).find(".translation-button")[0]
-            $(tb).css("visibility", "visible")
-
-        }
-
-
-        var backgroundVideo = $(slide).find(".backgroundvideo")
-        if (backgroundVideo.length != 0) {
-            let bckgrVidID = $(backgroundVideo[0]).attr("id")
-            if (bckgrVidID) {
-                let videosListBkgr = prepareVideos(backgroundVideo, { 'controls': [], 'settings': ['loop'], 'loop': { active: true }, 'clickToPlay': false, 'autoplay': true, 'muted': true, 'autopause': false })
-                videosListBkgr[0].volume = 0
-                videosList = videosList.concat(videosListBkgr);
+    new fullpage('#fullpage', {
+        fadingEffect: true,
+        fadingEffectKey: "dmVsdmV0c2tldGNoZXMubWVkaWEuYW1fbDRmWm1Ga2FXNW5SV1ptWldOMGxTNQ==",
+        verticalCentered: true,
+        lazyLoading: true,
+        afterLoad: function (origin, destination, direction) {
+            if (destination.item.id === "cover") {
+                initCoverPageEffects();
+            }
+            else {
+                $("#nav-arrow").show("slow");
             }
 
-        }
+            if (destination.item.id == "expertise") {
+                window.location.assign(EXPERTISEPAGE)
+            }
+
+            let slide = $("#" + destination.item.id);
+            //console.log(destination.item.id)
+            let customVideo = $(slide).find(".custom-video")
+            if (customVideo.length != 0) {
+                videosList = prepareVideos(customVideo)
+                videosList[0].on("timeupdate", function (evnt) {
+                    if (videosList[0].currentTime > 0 && videosList[0].currentTime > videosList[0].duration - 0.3) {
+                        $(slide).find(".plyr__control").hide()
+                        videosList[0].pause()
+                    }
+                })
+            }
 
 
-        var layeronbackgroundvideo = $(slide).find(".layeronbackgroundvideo")
-        if (layeronbackgroundvideo.length !== 0) {
-            adjustPersonSlide(layeronbackgroundvideo, true)
-        }
+            let fallVideo = $(slide).find(".fall-video")
+            if (fallVideo.length != 0) {
+                videosList = prepareVideos(fallVideo, { 'autopause': false }, false)
+            }
 
+            let personVideo = $(slide).find(".personVideo")
+            if (personVideo.length != 0) {
+                videosList = prepareVideos(personVideo, { 'autopause': false })
+            }
 
-        let quote = $(slide).find(".quote")
-        let quote_autor = $(slide).find(".quote-autor")
-        if (quote && quote_autor) {
-            $(quote[0]).addClass("fadeIn animation3")
-            setTimeout(function () {
-                $(quote_autor[0]).addClass("fadeIn animation3")
-            }, 1000)
-        }
+            if (LANG === "en") {
+                let tb = $(slide).find(".translation-button")[0]
+                $(tb).css("visibility", "visible")
 
+            }
 
-        let becouse = $(slide).find(".becouse")
-        let becouseList = $(slide).find(".becouse-list > li")
-        if (becouse && becouseList) {
-            $(becouse[0]).addClass("fadeIn animation2")
-            let ind = 8
-            let liIndex = 0
+            if (!IS_MOBILE) {
+                var backgroundVideo = $(slide).find(".backgroundvideo")
+                if (backgroundVideo.length != 0) {
+                    let bckgrVidID = $(backgroundVideo[0]).attr("id")
+                    if (bckgrVidID) {
+                        let videosListBkgr = prepareVideos(backgroundVideo, { 'controls': [], 'settings': ['loop'], 'loop': { active: true }, 'clickToPlay': false, 'autoplay': true, 'muted': true, 'autopause': false })
+                        videosListBkgr[0].volume = 0
+                        videosList = videosList.concat(videosListBkgr);
+                    }
 
-            let anim = setInterval(function () {
-                let li = becouseList[liIndex]
-                $(li).addClass("fadeIn")
-                $(li).addClass("animation" + ind)
-
-                ind += 4
-                liIndex++
-
-                if (liIndex == becouseList.length) {
-                    clearInterval(anim)
                 }
-
-            }, 1000)
-        }
-
-        let person = $(slide).find(".person-title")
-        let persontext = $(slide).find(".person-text")
-        if (person && persontext) {
-            $(person[0]).addClass("fadeIn animation3")
-            $(persontext[0]).addClass("fadeIn animation5")
-        }
+            }
 
 
-        let bkgrURL = $(slide).attr("data-background-image")
-        if (bkgrURL) {
-            $(slide).find(".backgroundvideo").css({ "opacity": 0.5 })
-            $(slide).css({
-                "background-image": 'url(' + bkgrURL + ')',
-                "background-size": "cover"
-            })
-        }
+            var layeronbackgroundvideo = $(slide).find(".layeronbackgroundvideo")
+            if (layeronbackgroundvideo.length !== 0) {
+                adjustPersonSlide(layeronbackgroundvideo, true)
+            }
 
-        let bordered = $(slide).find(".bordered")
-        bordered.addClass("fadeIn").addClass("animation2")
 
-        let relatives = $(slide).find(".relatives")
-        let relativesfade = $(slide).find(".relatives-fade")
+            let quote = $(slide).find(".quote")
+            let quote_autor = $(slide).find(".quote-autor")
+            if (quote && quote_autor) {
+                $(quote[0]).addClass("fadeIn animation3")
+                setTimeout(function () {
+                    $(quote_autor[0]).addClass("fadeIn animation3")
+                }, 1000)
+            }
 
-        if (relativesfade && relativesfade !== undefined) {
-            setTimeout(function () {
-                let ind = 0
-                $(relativesfade[ind]).addClass("fadeIn")
-                $(relativesfade[ind]).addClass("animation1")
-                ind++
 
-                let animIntrval = setInterval(function () {
+            let becouse = $(slide).find(".becouse")
+            let becouseList = $(slide).find(".becouse-list > li")
+            if (becouse && becouseList) {
+                $(becouse[0]).addClass("fadeIn animation2")
+                let ind = 8
+                let liIndex = 0
+
+                let anim = setInterval(function () {
+                    let li = becouseList[liIndex]
+                    $(li).addClass("fadeIn")
+                    $(li).addClass("animation" + ind)
+
+                    ind += 4
+                    liIndex++
+
+                    if (liIndex == becouseList.length) {
+                        clearInterval(anim)
+                    }
+
+                }, 1000)
+            }
+
+            let person = $(slide).find(".person-title")
+            let persontext = $(slide).find(".person-text")
+            if (person && persontext) {
+                $(person[0]).addClass("fadeIn animation3")
+                $(persontext[0]).addClass("fadeIn animation5")
+            }
+
+
+            let bkgrURL = $(slide).attr("data-background-image")
+            if (bkgrURL) {
+                $(slide).find(".backgroundvideo").css({ "opacity": 0.5 })
+                $(slide).css({
+                    "background-image": 'url(' + bkgrURL + ')',
+                    "background-size": "cover"
+                })
+            }
+
+            if (!IS_MOBILE) {
+            let bordered = $(slide).find(".bordered")
+            bordered.addClass("fadeIn").addClass("animation2")
+
+            let relatives = $(slide).find(".relatives")
+            let relativesfade = $(slide).find(".relatives-fade")
+
+            if (relativesfade && relativesfade !== undefined) {
+                setTimeout(function () {
+                    let ind = 0
                     $(relativesfade[ind]).addClass("fadeIn")
                     $(relativesfade[ind]).addClass("animation1")
-
                     ind++
 
-                    if (ind == relativesfade.length) {
-                        clearInterval(animIntrval)
-                    }
-                }, 250)
-            }, 500)
-        }
+                    let animIntrval = setInterval(function () {
+                        $(relativesfade[ind]).addClass("fadeIn")
+                        $(relativesfade[ind]).addClass("animation1")
 
-        if (relatives && relatives !== undefined) {
-            let ind = 0;
-            $(relatives[ind]).addClass("slideInDown")
-            $(relatives[ind]).addClass("animation1")
-            ind++
+                        ind++
 
-            let animIntrval = setInterval(function () {
+                        if (ind == relativesfade.length) {
+                            clearInterval(animIntrval)
+                        }
+                    }, 250)
+                }, 500)
+            }
 
-                $(relatives[ind]).addClass("slideInDown")
-                $(relatives[ind]).addClass("animation1")
+            
+                if (relatives && relatives !== undefined) {
+                    let ind = 0;
+                    $(relatives[ind]).addClass("slideInDown")
+                    $(relatives[ind]).addClass("animation1")
+                    ind++
 
-                ind++
-                if (ind == relatives.length) {
-                    clearInterval(animIntrval)
+                    let animIntrval = setInterval(function () {
+
+                        $(relatives[ind]).addClass("slideInDown")
+                        $(relatives[ind]).addClass("animation1")
+
+                        ind++
+                        if (ind == relatives.length) {
+                            clearInterval(animIntrval)
+                        }
+
+                    }, 200)
                 }
 
-            }, 200)
-        }
+            }
 
 
-        let trtext = $(slide).find(".translation-text")
-        if (trtext && trtext !== "undefined") {
-            $(slide).find(".translation-button").on("click", function () {
-                trtext.slideToggle(300);
-            })
-        }
+            let trtext = $(slide).find(".translation-text")
+            if (trtext && trtext !== "undefined") {
+                $(slide).find(".translation-button").on("click", function () {
+                    trtext.slideToggle(300);
+                })
+            }
 
 
-        let gallery = $(slide).find(".slider")
-        if (gallery && gallery !== undefined && gallery.length > 0) {
-            galleryes.push(initBxGallery(gallery))
-        }
+            let gallery = $(slide).find(".slider")
+            if (gallery && gallery !== undefined && gallery.length > 0) {
+                galleryes.push(initBxGallery(gallery))
+            }
 
 
-        /*
-        let zoomer = $(slide).find(".zoomer")
-        if(zoomer && zoomer.length>0){
-            relatives.each(function(i,rel){
-                beeIntervals.push(startBeeing(rel,false))
-            })
-            
-            relativesfade.each(function(i,rel){
-                beeIntervals.push(startBeeing(rel,true))
-            })
-          
-        }
-        */
+            /*
+            let zoomer = $(slide).find(".zoomer")
+            if(zoomer && zoomer.length>0){
+                relatives.each(function(i,rel){
+                    beeIntervals.push(startBeeing(rel,false))
+                })
+                
+                relativesfade.each(function(i,rel){
+                    beeIntervals.push(startBeeing(rel,true))
+                })
+              
+            }
+            */
 
 
-        //followSlideLine()
+            //followSlideLine()
 
-        /*let currentOnSlidline = $('*[data-hanum-anchor="'+destination.anchor+'"]')
-        //$("#slideLineArrow").css("")
-        let topOfArrow = currentOnSlidline.css("top")
-        console.log(topOfArrow)
-        $("#slideLineArrow").css("top",parseInt(topOfArrow)+TOPOFFSET+"px")
-        */
+            /*let currentOnSlidline = $('*[data-hanum-anchor="'+destination.anchor+'"]')
+            //$("#slideLineArrow").css("")
+            let topOfArrow = currentOnSlidline.css("top")
+            console.log(topOfArrow)
+            $("#slideLineArrow").css("top",parseInt(topOfArrow)+TOPOFFSET+"px")
+            */
 
-        //let topForArrow = calcDistance($("section"))
+            //let topForArrow = calcDistance($("section"))
 
-        let currentAnchor = $(destination.item).data("anchor")
-        if(openSlideline(currentAnchor)){
-            setTimeout(function(){
+            let currentAnchor = $(destination.item).data("anchor")
+            if (openSlideline(currentAnchor)) {
+                setTimeout(function () {
+                    slideLine()
+                }, TIME_BEFORE_SLIDELINE)
+
+            }
+            else {
                 slideLine()
-            }, TIME_BEFORE_SLIDELINE)
-            
+            }
+
+            updateLanguageLink(currentAnchor)
         }
-        else{
-            slideLine()
+
+        , onLeave: function (origin, destination, direction) {
+            for (ply of videosList) {
+                ply.destroy()
+            }
+            videosList = [];
+
+            scrollSound.play()
+            /*
+            for (beeInterv of beeIntervals) {
+                clearInterval(beeInterv)
+            }
+            beeIntervals = []
+            */
+
+
+
+            let slide = $("#" + origin.item.id);
+            $(slide).find(".translation-button").off("click")
+
+
+            let relatives = $(slide).find(".relatives")
+            let relativesfade = $(slide).find(".relatives-fade")
+            relatives.removeClass("slideInDown")
+            relativesfade.removeClass("fadeIn")
+
+            relatives.addClass("slideOutDown", function () {
+                setTimeout(function () {
+                    relatives.removeClass("slideOutDown")
+                }, 500)
+            })
+
+            closeToolTips()
+
+            galleryes = destroyBxGallery(galleryes)
+
         }
-        
-        updateLanguageLink(currentAnchor)
-    }
 
-    , onLeave: function (origin, destination, direction) {
-        for (ply of videosList) {
-            ply.destroy()
-        }
-        videosList = [];
-
-        scrollSound.play()
-        /*
-        for (beeInterv of beeIntervals) {
-            clearInterval(beeInterv)
-        }
-        beeIntervals = []
-        */
-
-
-
-        let slide = $("#" + origin.item.id);
-        $(slide).find(".translation-button").off("click")
-
-
-        let relatives = $(slide).find(".relatives")
-        let relativesfade = $(slide).find(".relatives-fade")
-        relatives.removeClass("slideInDown")
-        relativesfade.removeClass("fadeIn")
-
-        relatives.addClass("slideOutDown", function () {
-            setTimeout(function () {
-                relatives.removeClass("slideOutDown")
-            }, 500)
-        })
-
-        closeToolTips()
-
-        galleryes = destroyBxGallery(galleryes)
-
-    }
-
-});
-
+    });
+}, 3000)
 
 
 if (DEBUG) {
